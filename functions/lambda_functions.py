bf57b59b6067992f85fadf3ae8aac2dce2b9c931
Masatoshi Namiki <masa1824@gmail.com>
2023-11-08 13:19:26 +0000

    install poetry and create FastAPI application refs #7

--- functions/lambda_functions.py
+++ functions/lambda_functions.py
@@ -1,20 +1,24 @@
 import email
 
+DEF_TEST_MAIL = "./test/resources/ctq89s54b65oc9809dit427t13kqg6qp53c4kt01"
+
+
 def _analyze(_part):
     charset = _part.get_content_charset()
     payload = _part.get_payload(decode=True)
     body = payload.decode(charset)
     lines = body.splitlines()
     for line in lines:
-        if line.startswith('お届け予定日'):
+        if line.startswith("お届け予定日"):
             print(line)
-        if line.startswith('■'):
+        if line.startswith("■"):
             # 商品情報
             print(line)
 
-with open('./test/resources/ctq89s54b65oc9809dit427t13kqg6qp53c4kt01', 'r', encoding='UTF-8') as f:
+
+with open(DEF_TEST_MAIL, "r", encoding="UTF-8") as f:
     msg = email.message_from_file(f)
 
 for part in msg.walk():
-    if part.get_content_type() == 'text/plain':
+    if part.get_content_type() == "text/plain":
         _analyze(part)
